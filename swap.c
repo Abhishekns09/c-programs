022e639e3103447e84f8240f4ea14ab1d5b60b99
Abhishekns09 <abhishekns009@gmail.com>
2024-11-29 10:36:22 +0530

    Added new feature

--- swap.c
+++ swap.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 int main(){
+    printf("This program is about swaping a value without third variable");
     int a=10,b=20;
     printf("Before Swaping : %d  %d",&a,&b);
     a=a^b;
